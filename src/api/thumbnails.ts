64172ba6c628d774cdb16dd82d7612ab01d52927
Aleš Zima <zima.ales@seznam.cz>
2025-07-18 09:56:26 +0200

    Added storing thumbnail as base64 in sqlite

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,53 +1,46 @@
-import { getBearerToken, validateJWT } from "../auth";
-import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
-import type { ApiConfig } from "../config";
-import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { getBearerToken, validateJWT } from '../auth'
+import { respondWithJSON } from './json'
+import { getVideo, updateVideo } from '../db/videos'
+import type { ApiConfig } from '../config'
+import type { BunRequest } from 'bun'
+import { BadRequestError, NotFoundError, UserForbiddenError } from './errors'
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+const MAX_UPLOAD_SIZE = 10 << 20
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
+  const { videoId } = req.params as { videoId?: string }
   if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
+    throw new BadRequestError('Invalid video ID')
   }
 
-  const token = getBearerToken(req.headers);
-  const userID = validateJWT(token, cfg.jwtSecret);
+  const token = getBearerToken(req.headers)
+  const userID = validateJWT(token, cfg.jwtSecret)
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  console.log('uploading thumbnail for video', videoId, 'by user', userID)
 
-  // TODO: implement the upload here
+  const formData = await req.formData()
+  const file = formData.get('thumbnail')
+  if (!(file instanceof File)) {
+    throw new BadRequestError('Thumbnail file missing')
+  }
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError('Thumbnail exceeded file size limit')
+  }
+  const imgArrayBuffer = await file.arrayBuffer()
+  const stringedImgBuffer = Buffer.from(imgArrayBuffer).toString('base64')
+  const dataUrl = `data:${file.type};base64,${stringedImgBuffer}`
 
-  return respondWithJSON(200, null);
+  const video = getVideo(cfg.db, videoId)
+  if (!video) {
+    throw new NotFoundError('Video not found')
+  }
+
+  if (userID !== video.userID) {
+    throw new UserForbiddenError('Video not owned by this user')
+  }
+
+  video.thumbnailURL = dataUrl
+  updateVideo(cfg.db, video)
+
+  return respondWithJSON(200, video)
 }
